8fc292b8463c56ec89f4c161fe1de183543c5f61
Sanafar-coder <sanafar3870@gmail.com>
2025-06-28 23:34:31 +0530

    Update and rename DAY7/TASK1.java to SQL_TASK

--- DAY7/TASK1.java
+++
@@ -1,9 +0,0 @@
-Print all even numbers between 1 and 100:
-
-public class EvenNumbers {
-    public static void main(String[] args) {
-        for (int i = 2; i <= 100; i += 2) {
-            System.out.println(i);
-        }
-    }
-}
