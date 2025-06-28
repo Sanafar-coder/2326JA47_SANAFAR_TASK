f754a706dec9ced1fd65d052cfca60383b3270d1
Sanafar-coder <sanafar3870@gmail.com>
2025-06-28 20:34:45 +0530

    Create TASK3.java

---
+++ DAY2/TASK3.java
@@ -0,0 +1,20 @@
+Accept sentence ,split into words
+
+import java.util.Scanner;
+
+public class SplitSentence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String sentence = scanner.nextLine();
+        String[] words = sentence.split(" ");
+       
+        System.out.println("The words in the sentence are:");
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+        scanner.close();
+    }
+}
